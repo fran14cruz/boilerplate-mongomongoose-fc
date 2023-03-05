d0d395f8b01db2dec98f3cef78f01b6a9257f168
Francisco Cruz <fran14.ich@gmail.com>
2023-03-05 00:33:57 -0500

    Use model.findOne() to Return a Single Matching Document from Your Database

--- myApp.js
+++ myApp.js
@@ -50,8 +50,12 @@ const findPeopleByName = (personName, done) => {
   });
 };
 
+// Use model.findOne() to Return a Single Matching Document from Your Database
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ name: food }, function(err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
