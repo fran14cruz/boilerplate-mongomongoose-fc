701457815a6757a65a4d8d87b8b1925b6cd2898f
Francisco Cruz <fran14.ich@gmail.com>
2023-03-05 00:29:51 -0500

    Comments

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// create a Schema
 const personSchema = new Schema({
   name: {
     type: String,
